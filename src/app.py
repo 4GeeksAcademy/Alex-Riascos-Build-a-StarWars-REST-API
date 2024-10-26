dc20c6980da202a0f62ade47335f3abc5915cd6e
Alex Riascos <160887203+X4Nn3R@users.noreply.github.com>
2024-10-26 00:22:53 +0000

    se agrego las peticiones, GET, POST Y DELETE, Falta integrar favoritos

--- src/app.py
+++ src/app.py
@@ -3,13 +3,15 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
-from flask_swagger import swagger
-from flask_cors import CORS
+from flask_migrate import Migrate # type: ignore
+from flask_swagger import swagger # type: ignore
+from flask_cors import CORS # type: ignore
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Planet, Character, Vehicle
+
+
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +38,171 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+####################################
+# CRUD for User
+####################################
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    users= User.query.all()
+    users = list(map(lambda x: x.serialize(), users))
+    return jsonify(users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user_id(id):
+    user = User.query.get(id)
+    return jsonify(user.serialize()), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    request_body = request.get_json()
+    user = User(
+        username=request_body["username"], 
+        email=request_body["email"], 
+        password=request_body["password"], 
+        suscription_date=request_body["suscription_date"]        
+        )
+    #exmple: dict = {"username": "test", "email": "test", "password": "test", "suscription_date": "2021-09-01"}
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.serialize()), 200
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    if user is None:
+        raise APIException("User not found", status_code=404)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify(user.serialize()), 200
+
+####################################
+# CRUD for Planet
+####################################
+
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    planets= Planet.query.all()
+    planets = list(map(lambda x: x.serialize(), planets))
+    return jsonify(planets), 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_planet_id(id):
+    planet = Planet.query.get(id)
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planet', methods=['POST'])
+def create_planet():
+    request_body = request.get_json()
+    planet = Planet(
+        name=request_body["name"], 
+        population=request_body["population"], 
+        climate=request_body["climate"], 
+        terrain=request_body["terrain"], 
+        diameter=request_body["diameter"], 
+        rotation_period=request_body["rotation_period"], 
+        orbital_period=request_body["orbital_period"], 
+        gravity=request_body["gravity"], 
+        surface_water=request_body["surface_water"], 
+        created=request_body["created"]
+        )
+    #exmple: dict = {"name": "test", "population": 1, "climate": "test", "terrain": "test", "diameter": 1, "rotation_period": 1, "orbital_period": 1, "gravity": "test", "surface_water": 1, "created": "2021-09-01"}
+    db.session.add(planet)
+    db.session.commit()
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planet/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    planet = Planet.query.get(id)
+    if planet is None:
+        raise APIException("Planet not found", status_code=404)
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify(planet.serialize()), 200
+
+####################################
+# CRUD for Character
+####################################
+
+@app.route('/character', methods=['GET'])
+def get_character():
+    characters= Character.query.all()
+    characters = list(map(lambda x: x.serialize(), characters))
+    return jsonify(characters), 200
+
+@app.route('/character/<int:id>', methods=['GET'])
+def get_character_id(id):
+    character = Character.query.get(id)
+    return jsonify(character.serialize()), 200
+
+@app.route('/character', methods=['POST'])
+def create_character():
+    request_body = request.get_json()
+    character = Character(
+        name=request_body["name"], 
+        height=request_body["height"], 
+        mass=request_body["mass"], 
+        hair_color=request_body["hair_color"], 
+        age=request_body["age"], 
+        homeworld=request_body["homeworld"], 
+        species=request_body["species"]
+        )
+    #exmple: dict = {"name": "test", "height": 1, "mass": 1, "hair_color": "test", "age": 1, "homeworld": "test", "species": "test"}
+    db.session.add(character)
+    db.session.commit()
+    return jsonify(character.serialize()), 200
+
+@app.route('/character/<int:id>', methods=['DELETE'])
+def delete_character(id):
+    character = Character.query.get(id)
+    if character is None:
+        raise APIException("Character not found", status_code=404)
+    db.session.delete(character)
+    db.session.commit()
+    return jsonify(character.serialize()), 200
+
+####################################
+# CRUD for Vehicle
+####################################
+
+@app.route('/vehicle', methods=['GET'])
+def get_vehicle():
+    vehicles= Vehicle.query.all()
+    vehicles = list(map(lambda x: x.serialize(), vehicles))
+    return jsonify(vehicles), 200
+
+@app.route('/vehicle/<int:id>', methods=['GET'])
+def get_vehicle_id(id):
+    vehicle = Vehicle.query.get(id)
+    return jsonify(vehicle.serialize()), 200
+
+@app.route('/vehicle', methods=['POST'])
+def create_vehicle():
+    request_body = request.get_json()
+    vehicle = Vehicle(
+        name=request_body["name"], 
+        model=request_body["model"], 
+        manufacturer=request_body["manufacturer"], 
+        length=request_body["length"], 
+        max_atmosphering_speed=request_body["max_atmosphering_speed"], 
+        passengers=request_body["passengers"], 
+        cargo_capacity=request_body["cargo_capacity"], 
+        consumables=request_body["consumables"]
+        )
+    #exmple: dict = {"name": "test", "model": "test", "manufacturer": "test", "length": 1, "max_atmosphering_speed": 1, "passengers": 1, "cargo_capacity": 1, "consumables": "test"}
+    db.session.add(vehicle)
+    db.session.commit()
+    return jsonify(vehicle.serialize()), 200
+
+@app.route('/vehicle/<int:id>', methods=['DELETE'])
+def delete_vehicle(id):
+    vehicle = Vehicle.query.get(id)
+    if vehicle is None:
+        raise APIException("Vehicle not found", status_code=404)
+    db.session.delete(vehicle)
+    db.session.commit()
+    return jsonify(vehicle.serialize()), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,12 +1,37 @@
+# This file will contain the models for the database.
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Table
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    suscription_date = db.Column(db.Date(), unique=False, nullable=False)    
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __init__(self, username, email, password, suscription_date):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.suscription_date = suscription_date
+        self.is_active = True
+    
+
+    # favorite_planet_id = db.Column(db.Integer, db.ForeignKey('favorite_planet.id'))
+    # favorite_planet = db.relationship('Favorite_Planet', backref='users', foreign_keys=[favorite_planet_id])
+    
+    # favorite_character_id = db.Column(db.Integer, db.ForeignKey('favorite_character.id'))
+    # favorite_character = db.relationship('Favorite_Character', backref='users', foreign_keys=[favorite_character_id])
+    
+    # favorite_vehicle_id = db.Column(db.Integer, db.ForeignKey('favorite_vehicle.id'))
+    # favorite_vehicle = db.relationship('Favorite_Vehicle', backref='users', foreign_keys=[favorite_vehicle_id])
+    
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +39,175 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+            "suscription_date": self.suscription_date,
             # do not serialize the password, its a security breach
         }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    climate = db.Column(db.String(120), unique=False, nullable=False)
+    terrain = db.Column(db.String(120), unique=False, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=False)
+    rotation_period = db.Column(db.Integer, unique=False, nullable=False)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=False)
+    gravity = db.Column(db.String(120), unique=False, nullable=False)
+    surface_water = db.Column(db.Integer, unique=False, nullable=False)
+    created = db.Column(db.DateTime(), unique=False, nullable=False)
+
+    def __init__(self, name, population, climate, terrain, diameter, rotation_period, orbital_period, gravity, surface_water, created):
+        self.name = name
+        self.population = population
+        self.climate = climate
+        self.terrain = terrain
+        self.diameter = diameter
+        self.rotation_period = rotation_period
+        self.orbital_period = orbital_period
+        self.gravity = gravity
+        self.surface_water = surface_water
+        self.created = datetime.now(timezone.utc)
+     
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "surface_water": self.surface_water,
+            "created": self.created
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=False)
+    mass = db.Column(db.Integer, unique=False, nullable=False)
+    hair_color = db.Column(db.String(120), unique=False, nullable=False )
+    age = db.Column(db.Integer, unique=False, nullable=False)
+    homeworld = db.Column(db.String(120), unique=False, nullable=False)
+    species = db.Column(db.String(120), unique=False, nullable = False)
+
+    def __init__(self, name, height, mass, hair_color, age, homeworld, species):
+        self.name = name
+        self.height = height
+        self.mass = mass
+        self.hair_color = hair_color
+        self.age = age
+        self.homeworld = homeworld
+        self.species = species
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "age": self.age,
+            "homeworld": self.homeworld,
+            "species": self.species
+        }
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicle'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    model = db.Column(db.String(120), unique=False, nullable=False)
+    manufacturer = db.Column(db.String(120), unique=False, nullable=False)
+    length = db.Column(db.Integer, unique=False, nullable=False)
+    max_atmosphering_speed = db.Column(db.Integer, unique=False, nullable=False)    
+    passengers = db.Column(db.Integer, unique=False, nullable=False) #
+    cargo_capacity = db.Column(db.Integer, unique=False, nullable=False)
+    consumables = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __init__(self, name, model, manufacturer, length, max_atmosphering_speed, passengers, cargo_capacity, consumables):
+        self.name = name
+        self.model = model
+        self.manufacturer = manufacturer
+        self.length = length
+        self.max_atmosphering_speed = max_atmosphering_speed
+        self.passengers = passengers
+        self.cargo_capacity = cargo_capacity
+        self.consumables = consumables      
+    
+    def __repr__(self):
+        return '<Vehicle %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "length": self.length,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables
+        }
+
+# class Favorite_Planet(db.Model):
+#     __tablename__ = 'favorite_planet'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+
+#     def __repr__(self):
+#         return '<Favorite_Planet %r>' % self.id
+    
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "user_id": self.user_id,
+#             "planet_id": self.planet_id
+#         }
+
+# class Favorite_Character(db.Model):
+#     __tablename__ = 'favorite_character'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+
+#     def __repr__(self):
+#         return '<Favorite_Character %r>' % self.id
+    
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "user_id": self.user_id,
+#             "character_id": self.character_id
+#         }
+    
+# class Favorite_Vehicle(db.Model):
+#     __tablename__ = 'favorite_vehicle'
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
+
+#     def __repr__(self):
+#         return '<Favorite_Vehicle %r>' % self.id
+    
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "user_id": self.user_id,
+#             "vehicle_id": self.vehicle_id
+#         }
+
